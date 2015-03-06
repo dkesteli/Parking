72b3cf20563390bb7408064c73872596c8449077
dkesteli <dkesteli@ulb.ac.be>
2015-03-06 19:26:55 +0100

    update client

--- Parking/src/parking/Client.java
+++ Parking/src/parking/Client.java
@@ -1,5 +1,26 @@
 package parking;
 
 public class Client {
-
+	private String nom;
+	private String prenom;
+	private int id;
+	private String mail;
+	private static int cptId = 0;
+	
+	public Client() {
+		this.nom = "";
+		this.prenom = "";
+		this.id = ++cptId;
+		this.mail = "";
+	}
+	
+	public Client(String nom, String prenom, String mail) {
+		this.nom = nom;
+		this.prenom = prenom;
+		this.mail = mail;
+	}
+	
+	public String getNom() {
+		return this.nom;
+	}
 }
