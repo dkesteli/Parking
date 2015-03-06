b9fbeb680d6d6a43b0f0dc3ba087648ac1d7f78f
Maxime <Maxime@192.168.10.132>
2015-03-06 19:44:33 +0100

    classe Parking avec contructeur et getters

---
+++ Parking/src/parking/Parking.java
@@ -0,0 +1,34 @@
+package parking;
+
+public class Parking {
+	
+	private String idParking;
+	private String adresse;
+	private int capacite;
+	private int placesLibres;
+	
+	public Parking(String idParking,String adresse, int capacite){
+		this.adresse=adresse;
+		this.capacite=capacite;
+		this.idParking=idParking;
+		this.placesLibres=capacite; //à l'initialisation toutes les places sont libres
+	}
+	
+	public String getId(){
+		return idParking;
+	}
+	
+	public String getAdresse(){
+		return adresse;
+	}
+	
+	public int getCapacite(){
+		return capacite;
+	}
+	
+	public int getPlacesLibres(){
+		return placesLibres;
+	}
+	
+
+}
